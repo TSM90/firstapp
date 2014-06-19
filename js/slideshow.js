d651369abcef5a9975499422fe497181bbab1efa
tsm90 <thomas_sundstrom90@live.se>
2014-06-19 14:40:46 +0200

    slideshow.js är finslipade kodutseende mässigt

--- js/slideshow.js
+++ js/slideshow.js
@@ -3,13 +3,13 @@ var image = new Array("image/1633743.jpg", "image/sjobod_moljen_gladahudik.jpg",
 		var image_number = 0;
 		var image_Length = image.length -1;
 
-		function change_image( num ){
+		function change_image( num ) {
 			image_number = image_number + num;
 
 			if(image_number > image_Length){
 				image_number = 0;
 			}
-			if(image_number < 0){
+			if(image_number < 0) {
 				image_number = image_Length;
 			}
 			document.slideshow.src= image[image_number];
